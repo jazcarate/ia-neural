9afcb918fe2ce318182c35d457bbabae5123778c
J Florius <j@florius.com.ar>
2017-10-12 00:44:50 -0300

    inner layer

--- src/learn.cr
+++ src/learn.cr
@@ -5,38 +5,51 @@ log = Logger.new(STDOUT)
 log.level = Logger::ERROR
 
 
-input = Matrix(Float64).from [[5.1, 3.5, 1.4, 0.2],
-                              [4.9, 3.0, 1.4, 0.2],
-                              [6.2, 3.4, 5.4, 2.3],
-                              [5.9, 3.0, 5.1, 1.8]]
+input = Matrix(Float64).from [[0.0,0.0,1.0],
+                              [0.0,1.0,1.0],
+                              [1.0,0.0,1.0],
+                              [1.0,1.0,1.0]]
 
 results = Matrix(Float64).from [[0.0],
-                                [0.0],
                                 [1.0],
-                                [1.0]]
+                                [1.0],
+                                [0.0]]
 
-weights = Matrix(Float64).new 4,1 {0.5}
+syn0 = Matrix(Float64).new 3,4 {Random.rand}
+syn1 = Matrix(Float64).new 4,1 {Random.rand}
 
 gen = 1
-while gen < 10000
+while gen < 60000
   log.info("--- Generation #{gen} ---")
 
-  pred = (input.x weights).map(LearningMaths.sigmoid)
-  log.info("Prediction:\t\t #{pred.to_s}")
+  l0 = input
+  l1 = (l0.x syn0).map(LearningMaths.sigmoid)
+  l2 = (l1.x syn1).map(LearningMaths.sigmoid)
+  log.info("Prediction:\t\t #{l2.to_s}")
 
-  pred_error = results - pred
-  log.info("Prediction error:\t #{pred_error.to_s}")
+  l2_error = results - l2
+  log.info("Prediction layer 2 error:\t #{l2_error.to_s}")
 
-  pred_delta = pred_error * pred.map(LearningMaths.sigmoid_d)
-  log.info("Prediction delta:\t #{pred_delta.to_s}")
+  if gen % 10000 == 0
+    puts l2_error.clone.flatten.map(&.abs).sum
+  end
 
-  weights_delta = input.transpose.x pred_delta
-  log.info("weights delta:\t #{weights_delta.to_s}")
+  l2_delta = l2_error * l2.map(LearningMaths.sigmoid_d)
+  log.info("Prediction layer 2 delta:\t #{l2_delta.to_s}")
 
-  weights = weights + weights_delta
-  log.info("new weights:\t #{weights.to_s}")
+  l1_error = l2_delta.x syn1.t
+  log.info("Prediction layer 1 error:\t #{l1_error.to_s}")
+
+  l1_delta = l1_error * l1.map(LearningMaths.sigmoid_d)
+  log.info("Prediction layer 1 delta:\t #{l1_delta.to_s}")
+
+
+  syn1 += l1.t.x l2_delta
+  syn0 += l0.t.x l1_delta
+  log.info("new syn0:\t #{syn0.to_s}")
+  log.info("new syn1:\t #{syn1.to_s}")
 
   gen += 1
 end
 
-puts weights.to_s
+puts l2.to_s
